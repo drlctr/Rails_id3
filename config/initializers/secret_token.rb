0bc5e3c0c92a30f02bc68f7af417f5af34e0f54d
Neil Woodward <drlctr@gmail.com>
2014-04-08 21:22:15 -0700

    initial commit.  Rails,Gemfile, rspec, rvm version & gemfile, rspec install.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Id3Mgt::Application.config.secret_key_base = '73d139b66aee3c47b7e2c5aa96448f9173ee249e97dc329dbfc0e55c770383655205b8a5e60d855e2b7d53f2da9f2a9fc989cc6d34ca79e9e6851031bdbba7b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Id3Mgt::Application.config.session_store :cookie_store, key: '_id3_mgt_session'
