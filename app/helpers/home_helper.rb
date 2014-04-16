e93e01e5bb7a7a8dc4a56721d418c9a34012c0c4
Neil Woodward <drlctr@gmail.com>
2014-04-15 21:01:53 -0700

    Homework 2 complete.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,12 +1,16 @@
 module HomeHelper
 
-	def pick_guitar
-
-		if @color_white
+	def pick_guitar(color_wht)
+		color_wht_b = to_boolean(color_wht)
+		if color_wht_b
 			@image = 'FlyingVW.jpg' 
 		else
 			@image = 'FlyingVN.jpg'
 		end
+	end
+
+	def to_boolean(str)
+		str == 'true'
 	end
 
 end
